03eddb7ffccd88955646ba318a3c2788fe45d374
Ян <y1nqq@yandex.ru>
2024-12-23 22:10:31 +0300

    Добавлен новый тест для функции add

--- test_app.py
+++ test_app.py
@@ -1,7 +1,10 @@
-# test_app.py
 from app import add
 
 def test_add():
+    # Существующие тесты
     assert add(1, 2) == 3
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
+    
+    # Новый тест
+    assert add(-3, -5) == -8  # Новый тест для суммы отрицательных чисел
